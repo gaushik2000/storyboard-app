56e157e5b6b2dfa65792e31199fde7a2b2bf4aef
gaushik2000 <gaushik2000@gmail.com>
2026-03-19 21:40:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const callClaude = async (userMessage) => {
     method: "POST",
     headers: {
   "Content-Type": "application/json",
-  "x-api-key": "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",
+  "x-api-key": "",
   "anthropic-version": "2023-06-01",
   "anthropic-dangerous-direct-browser-access": "true",
 },
